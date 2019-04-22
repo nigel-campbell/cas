a5648dbfe4f9bc3f344632941a70f284c25e422a
Nigel Campbell <ncampbell7@gatech.edu>
2019-04-22 17:02:35 -0400

    More progress on unmarshalling XML elements.

--- attribute_test.go
+++ attribute_test.go
@@ -18,8 +18,9 @@ type SoapBody struct {
 }
 
 type SoapResponse struct {
-	XMLName xml.Name `xml:Response"`
-	Status  SoapStatus
+	XMLName   xml.Name `xml:Response"`
+	Status    SoapStatus
+	Assertion SoapAssertion
 }
 
 type SoapStatus struct {
@@ -32,6 +33,38 @@ type SoapStatusCode struct {
 	Value   string   `xml:"Value,attr"`
 }
 
+type SoapAssertion struct {
+	XMLName                 xml.Name `xml:Assertion`
+	Conditions              SoapConditions
+	AuthenticationStatement SoapAuthenticationStatement
+	AttributeStatement      SoapAttributeStatement
+}
+
+type SoapConditions struct {
+	XMLName xml.Name `xml:Conditions`
+}
+
+type SoapAuthenticationStatement struct {
+	XMLName xml.Name `xml:AuthenticationStatement`
+}
+
+type SoapAttributeStatement struct {
+	XMLName xml.Name `xml:AttributeStatement`
+	// Subject    SAMLSubject
+	Attributes []SAMLAttribute
+}
+
+type SAMLSubject struct {
+	XMLName        xml.Name `xml.Subject`
+	NameIdentifier string   `xml:NameIdentifier`
+}
+
+type SAMLAttribute struct {
+	XMLName        xml.Name `xml.Attribute`
+	AttributeName  string   `xml:"AttributeName,attr"`
+	AttributeValue string   `xml:AttributeValue`
+}
+
 func TestAttributeMarshall(t *testing.T) {
 	file, _ := ioutil.ReadFile("./_examples/response_pretty.xml")
 
@@ -41,5 +74,5 @@ func TestAttributeMarshall(t *testing.T) {
 	if err != nil {
 		fmt.Printf("%v\n", err)
 	}
-	fmt.Printf("%v\n", envelope.Body)
+	fmt.Printf("%v\n", envelope.Body.Response.Assertion.AttributeStatement)
 }
